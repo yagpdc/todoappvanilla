f583456d6ed0a752e13f72c87a124226b6318509
Yago <ysantanaf@gmail.com>
2023-10-11 01:59:21 -0300

    add: organizacao do projeto

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-import { setupCounter } from './helpers/counter.ts'
+import  {helloWorld}  from './helpers/addtask';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/`
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('#form_wrapper');
+  form?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    helloWorld();
+  });
+});
