05be9bcc98eb7b169da322f8ff15801dfca7b271
Yago <ysantanaf@gmail.com>
2023-10-15 00:52:18 -0300

    add: pomodoro with timer and buttons

--- src/helpers/pomodoro.ts
+++ src/helpers/pomodoro.ts
@@ -1,18 +1,108 @@
-function pomodoro(){
-    let container = document.querySelector('#html-container')
-      
-    let time =/*html*/`
-    <div class="time-container">
-      <div class="time">
-        <span id="minutes">25</span>
-          <span>:</span>
-        <span id="seconds">00</span>
-      </div>
-    </div>
-    `
-    if(container){
-      container.innerHTML = time
-    }
-    }
+import ring  from "/ring.mp3";
 
-export {pomodoro}
+let container = document.querySelector('#html-container')
+let pomodoroTime: HTMLInputElement | null = document.querySelector('#work')
+let audio = new Audio(ring)
+let breakTime: HTMLInputElement | null = document.querySelector('#short')
+let shortLabel = document.querySelector('#shortLabel')
+let workLabel = document.querySelector('#workLabel')
+
+
+function pomodoro(){
+    
+
+    let time = /*html*/ `
+        <div class="time-container">
+            <div class="time">
+                <span id="minutes">${pomodoroTime?.value}</span>
+                <span>:</span>
+                <span id="seconds">00</span>
+            </div>
+        </div>
+    `
+
+    if(container){
+        container.innerHTML = time
+    }
+}
+
+
+
+let myInterval: number | null = null;
+let tracker: boolean = true
+
+function start() {
+    
+    let minutes = document.querySelector('#minutes') as HTMLSpanElement 
+    let seconds = document.querySelector('#seconds')
+    workLabel!.classList.add('highlight')
+
+    
+    
+    if(myInterval !== null){
+        console.log('Já tem um intervalo')
+    } else if(myInterval === null) {
+        myInterval = setInterval(function() {
+            if(seconds?.innerHTML !== "00"){
+                seconds!.innerHTML = (parseInt(seconds!.innerHTML) - 1).toString().padStart(2, '0')
+            } else if(minutes?.innerHTML !== "00" && seconds?.innerHTML === "00"){
+                minutes!.innerHTML = (parseInt(minutes!.innerHTML) - 1).toString().padStart(2, '0')
+                seconds!.innerHTML = "59"
+            } else if (tracker === false){
+                console.log('Pomodoro time')
+                shortLabel!.classList.remove('highlight')
+                workLabel!.classList.add('highlight')
+                audio.play()
+                setTimeout(() => {
+                    audio.pause();
+                    audio.currentTime = 0; 
+                }, 3000);
+                minutes!.innerHTML = pomodoroTime!.value
+                seconds!.innerHTML = "00"
+                clearInterval(myInterval!)
+                myInterval = null
+                tracker = !tracker
+                console.log(tracker)
+            } else if (tracker === true){
+                console.log('Break time')
+                workLabel!.classList.remove('highlight')
+                shortLabel!.classList.add('highlight')
+                audio.play()
+                setTimeout(() => {
+                    audio.pause();
+                    audio.currentTime = 0; 
+                }, 3000);
+                minutes!.innerHTML = breakTime!.value
+                seconds!.innerHTML = "00"
+                clearInterval(myInterval!)
+                myInterval = null
+                tracker = !tracker
+                console.log(tracker)
+            }
+
+        }, 1000);
+    }
+}
+
+function stop() {
+    console.log('Stop');
+    if (myInterval !== null) {
+        clearInterval(myInterval);
+        myInterval = null;
+    }
+}
+
+function reset(){
+    let minutes = document.querySelector('#minutes') as HTMLSpanElement 
+    let seconds = document.querySelector('#seconds')
+    if (myInterval !== null) {
+        clearInterval(myInterval);
+        myInterval = null;
+    }
+    minutes!.innerHTML = pomodoroTime!.value
+    seconds!.innerHTML = "00"
+}
+
+
+
+export {pomodoro, start, stop, reset}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import  { addtask}  from './helpers/addtask';
 import { calendar } from './helpers/calendar';
-import { pomodoro } from './helpers/pomodoro';
+import { pomodoro, start, stop, reset } from './helpers/pomodoro';
 
   const form = document.querySelector('#form_wrapper');
   form?.addEventListener('submit', function (event) {
@@ -10,18 +10,19 @@ import { pomodoro } from './helpers/pomodoro';
   
 
   const send = document.querySelector('#send');
-
   send?.addEventListener('click', function (event) {
     event.preventDefault();
     addtask();
   }
   );
 
-calendar() 
+calendar()
 
-
-
-
-
-
-pomodoro()
+const startPomodoro = document.querySelector('#start') 
+const workInput = document.querySelector('#work') as HTMLInputElement
+const stopPomodoro = document.querySelector('#pause')
+const resetPomodoro = document.querySelector('#reset')
+workInput?.addEventListener('change', pomodoro)
+startPomodoro?.addEventListener('click', start)
+stopPomodoro?.addEventListener('click', stop)
+resetPomodoro?.addEventListener('click', reset)
